934386e07d905eaa172d454d8e19bcdd0bf9c47e
MauriceStrickland <maurice.strickland@viasat.com>
2017-05-10 16:55:05 -0700

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,6 @@
 var socket = io();
 socket.on('connect', function() {
   console.log('connected to server');
-  
-  socket.emit('createMessage', {
-    from: "Bill",
-    text: "Hello, its me"
-  });
 });
 
 socket.on('newMessage', function(newMessgae){
--- server/server.js
+++ server/server.js
@@ -19,10 +19,10 @@ io.on('connection', (socket) => {
   socket.on('createMessage', (message) => {
     console.log('Message: ', message);
     
-    socket.emit('newMessage', {
+    io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createdAt: Date.now()
+      createdAt: new Date().getTime()
     });
     
   });
