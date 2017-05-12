6bf44a2b3350944b226e0760d3a34f1a0a1e2bfd
MauriceStrickland <maurice.strickland@viasat.com>
2017-05-11 20:11:20 -0700

    Added style for chat

--- public/js/index.js
+++ public/js/index.js
@@ -31,30 +31,35 @@ socket.on('disconnect', function() {
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
   
+  var messageTextbox =jQuery('[name=message]');
+  
   socket.emit('createMessage', {
     from:'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   }, function(){
-    
+    messageTextbox.val('');
   });
 });
 
 
 var locationButton = jQuery('#send-location');
-
-
 locationButton.on('click', function() {
   if(!navigator.geolocation){
     return alert("Geolocation not supported by your browser");
   }
+  
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
   
   navigator.geolocation.getCurrentPosition(function (position) {
     socket.emit('createLocationMessage', {
       lat: position.coords.latitude,
       long: position.coords.longitude
     });
+    
+    locationButton.removeAttr('disabled').text('Send location');
   }, function() {
     alert("Unable to fecth location");
+    locationButton.removeAttr('disabled').text('Send location');
   });
 });
 
